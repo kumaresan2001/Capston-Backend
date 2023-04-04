c1dbb1f7fd5180019a082e08504e9b6e28b2175a
kumaresan2001 <kumaresancrseven@gmail.com>
2023-04-04 11:02:01 +0530

    update

---
+++ controllers/admin/user/switchToUser.js
@@ -0,0 +1,30 @@
+const { UserDetails } = require("../../../models/userModel");
+
+const switchToUser = async (req, res) => {
+  try {
+    const { e } = req.body;
+    const doc = await UserDetails.findById(req.params.id);
+    if (doc.email == "siva@gmail.com") {
+      res.json({
+        statusCode: 401,
+        message: "Default Admin Account, So Roll Does Not Update ",
+      });
+    } else {
+      doc.isAdmin = e;
+      await doc.save();
+      res.json({
+        statusCode: 200,
+        message: "Roll Update Successfully ",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      statusCode: 500,
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
+
+module.exports = { switchToUser };
